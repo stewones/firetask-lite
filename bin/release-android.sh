87e4962d41c6b91041f66e43889ad5d6763e7e9e
st1s <hi@st1s.io>
2016-12-19 13:37:25 -0200

    chore(): update some stuff

--- bin/release-android.sh
+++ bin/release-android.sh
@@ -1,2 +1,3 @@
 # build and zipalign for android
+# just execute `npm run release:android` from the `lite` folder
 cd ../lite && cordova plugin rm cordova-plugin-console && cordova build --release android && cordova plugin add cordova-plugin-console && cp platforms/android/build/outputs/apk/android-release-unsigned.apk android-release-unsigned.apk && jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore firetask-lite.keystore android-release-unsigned.apk firetask-lite && rm -rf firetask-lite.apk && zipalign -v 4 android-release-unsigned.apk firetask-lite.apk
